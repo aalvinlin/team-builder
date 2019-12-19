510de215108efe3620194b293f39a04e8b6ce6a5
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2019-12-19 09:08:06 -0800

    Fixed capitalization error with emtpy string as role

--- team-builder/src/components/MemberCard.js
+++ team-builder/src/components/MemberCard.js
@@ -8,11 +8,18 @@ function MemberCard(props) {
     let cardColors = "green blue pink orange yellow".split(" ");
     let cardColor = cardColors[props.member.id % (cardColors.length)];
 
+    let roleCapitalized = member.role;
+
+    if (member.role.length > 1)
+    {
+        roleCapitalized = member.role[0].toUpperCase() + member.role.slice(1, member.role.length);
+    }
+
     return (
 
         <div className={"memberCard " + cardColor} key={member.name}>
             <h3 className="memberName">{member.name}</h3>
-            <p className="memberRole">{member.role[0].toUpperCase() + member.role.slice(1, member.role.length)}</p>
+            <p className="memberRole">{roleCapitalized}</p>
             <p className="memberEmail">{member.email}</p>
             <p className="edit"><a href="#" onClick={editFunction}>(edit)</a></p>
         </div>
